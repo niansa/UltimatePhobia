36aef8035e68992876c599030605e6d145ca0fa7
niansa <tuxifan@posteo.de>
2025-01-04 14:08:48 +0100

    Extended blacklist with files created by ultimatephobia

--- UltimatePhobia/disable_antimod.cpp
+++ UltimatePhobia/disable_antimod.cpp
@@ -27,7 +27,7 @@ static bool maybeForbiddenFile(std::string path, bool dllSearch = false) {
     std::transform(path.begin(), path.end(), path.begin(),::tolower);
 
     // Check blacklist
-    for (std::string_view term : {".exe", ".dll", "melon", "bepin", "doorstop", "dotnet", "mono", "coreclr", "bootstrap"}) {
+    for (std::string_view term : {".exe", ".dll", "melon", "bepin", "doorstop", "dotnet", "mono", "coreclr", "bootstrap", "up_log.txt", "up_log.txt", "imgui.ini"}) {
         if (dllSearch && term[0] == '.')
             continue;
         if (path.find(term) != path.npos)
