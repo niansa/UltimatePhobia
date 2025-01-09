f680af723b03a89b2795a39567f0a41f6a3d0afc
niansa <tuxifan@posteo.de>
2025-01-09 20:19:56 +0100

    Added empty "Fixes" mod

---
+++ UltimatePhobia/mods/fixes.cpp
@@ -0,0 +1,8 @@
+#include "fixes.hpp"
+#include "gamedata.hpp"
+#include "game_types.hpp"
+#include "bindings/unityengine.hpp"
+
+
+
+Fixes::Fixes() {}
---
+++ UltimatePhobia/mods/fixes.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "base.hpp"
+#include "game_hook.hpp"
+
+
+class Fixes final : public Mod {
+public:
+    Fixes();
+};
+
+extern ModInfo fixesInfo;
