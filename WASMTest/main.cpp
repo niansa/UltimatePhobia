22264670a51fcb514cbdba8e0ec3caa5bd6eb69f
niansa <tuxifan@posteo.de>
2025-01-16 22:47:41 +0100

    In WASMTest example, use _cs for all string literals

--- WASMTest/main.cpp
+++ WASMTest/main.cpp
@@ -7,11 +7,11 @@ using namespace Helpers::Literals;
 
 
 WASM_EXPORT void onLoad() {
-    logInfo(toCsString("Hello from WASM module!"));
+    logInfo("Hello from WASM module!"_cs);
     Helpers::call<"void UnityEngine_Debug__Log (Il2CppObject* message, const MethodInfo* method);">("Hello world from WASM module!"_cs, nullptr);
 }
 WASM_EXPORT void onUnload() {
-    logInfo(toCsString("Goodbye from WASM module :-("));
+    logInfo("Goodbye from WASM module :-("_cs);
     Helpers::call<"void UnityEngine_Application__Quit (int32_t exitCode, const MethodInfo* method);">(0, nullptr);
 }
 
