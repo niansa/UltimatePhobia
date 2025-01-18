0a6f4285efa9487a383631aeba955237ffa2e4a2
niansa <tuxifan@posteo.de>
2025-01-18 01:31:05 +0100

    Greatly extended WASMTest

--- WASMTest/main.cpp
+++ WASMTest/main.cpp
@@ -1,18 +1,86 @@
+#include <stdio.h>
 #include <wasm32sdk.h>
 #include "wasm_interface.hpp"
 
 using namespace WASMInterface;
+using namespace Helpers;
 using namespace Helpers::Literals;
 
+namespace {
+ObjectHandle fuseBox;
+int useBoxCountDown = -1;
+bool originalState = true;
+}
 
 
-WASM_EXPORT void onLoad() {
+
+void useBoxSoonIfNeeded() {
+    printf("Countdown: %i\nOriginal state: %s\n", useBoxCountDown, originalState?"true":"false");
+    if (!originalState) {
+        logInfo("Retriggering fuse box soon..."_cs);
+        useBoxCountDown = 200;
+    } else {
+        useBoxCountDown = -1;
+    }
+}
+
+
+WASM_EXPORT("onLoad")
+void onLoad() {
     logInfo("Hello from WASM module!"_cs);
-    Helpers::call<"void UnityEngine_Debug__Log (Il2CppObject* message, const MethodInfo* method);">("Hello world from WASM module!"_cs, nullptr);
+    puts("Hello from WASM puts!\nThis is a multi-line test...");
+    printf("Hello from WASM printf at %p!\nThis is another multi-line test...\n", printf);
+    call<"void UnityEngine_Debug__Log (Il2CppObject* message, const MethodInfo* method);">("Hello world from WASM module!"_cs, nullptr);
+
+    hook(getMethodCached<"FuseBox$$UseNetworked">(), "onFuseBoxUseNetworked");
+    hook(getMethodCached<"Photon.Pun.PhotonNetwork$$CreateRoom">(), "onCreateRoom");
 }
-WASM_EXPORT void onUnload() {
+WASM_EXPORT("onUnload")
+void onUnload() {
     logInfo("Goodbye from WASM module :-("_cs);
-    Helpers::call<"void UnityEngine_Application__Quit (int32_t exitCode, const MethodInfo* method);">(0, nullptr);
+    unhook(getMethodCached<"FuseBox$$UseNetworked">());
+    unhook(getMethodCached<"Photon.Pun.PhotonNetwork$$CreateRoom">());
+    dropObject(fuseBox);
 }
 
-WASM_EXPORT void onUiUpdate() {}
+WASM_EXPORT("onUiUpdate")
+void onUiUpdate() {
+    if (useBoxCountDown == 0) {
+        logInfo("Triggering fuse box..."_cs);
+        call<"FuseBox$$Use">(fuseBox, nullptr);
+    } else if (useBoxCountDown > 0) {
+        --useBoxCountDown;
+    }
+}
+
+
+WASM_EXPORT("onFuseBoxUseNetworked")
+void onFuseBoxUseNetworked() {
+    // Get new fuse box object (which is passed as first argument)
+    dropObject(fuseBox);
+    fuseBox = getArg<ObjectHandle>(0);
+
+    // Call original function
+    call(getOriginal(), getArgCount());
+    logInfo("Fuse box triggered."_cs);
+
+    // Use fuse box again later if not triggered manually
+    originalState = !originalState;
+    useBoxSoonIfNeeded();
+}
+
+WASM_EXPORT("onCreateRoom")
+void onCreateRoom() {
+    // Get original room name
+    auto originalName = getArg<ObjectHandle>(0);
+
+    // Replace room name
+    addArg("WASM Test Mod user"_cs);
+    moveArg(0);
+
+    // Call origin function
+    call(getOriginal(), getArgCount());
+    printf("Room created with replacement name instead of \"%s\"!\n", getCString(originalName));
+
+    dropObject(originalName);
+}
---
+++ WASMTest/user_impl.cpp
@@ -0,0 +1,51 @@
+#include "wasm_interface.hpp"
+
+#include <user-impl.h>
+
+using namespace WASMInterface;
+using namespace Helpers;
+using namespace Helpers::Literals;
+
+
+
+extern "C" {
+namespace {
+ObjectHandle buffer = createCsString();
+}
+
+static void append_buffer(ObjectHandle str) {
+    ObjectHandle new_buffer = call<"System_String_o* System_String__Concat (System_String_o* str0, System_String_o* str1, const MethodInfo* method);", ObjectHandle>(buffer, str, nullptr);
+    dropObject(buffer);
+    buffer = new_buffer;
+}
+static void flush_buffer() {
+    logInfo(buffer);
+    dropObject(buffer);
+    buffer = createCsString();
+}
+
+void _putchar(char c) {
+    if (c == '\n')
+        flush_buffer();
+    else
+        append_buffer(toCsStringWithLength(&c, 1));
+}
+void _puts(const char *str) {
+    const char *remaining = nullptr;
+    size_t length = 0;
+
+    for (; str[length]; ++length) {
+        if (str[length] == '\n') {
+            remaining = str + length + 1;
+            break;
+        }
+    }
+
+    append_buffer(toCsStringWithLength(str, length));
+    if (remaining) {
+        flush_buffer();
+        return _puts(remaining);
+    }
+    flush_buffer();
+}
+}
