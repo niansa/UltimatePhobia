87da249bf109c3725f10082016b2f31b2ff99c36
niansa <tuxifan@posteo.de>
2025-01-29 15:37:37 +0100

    Added WASM AOT compilation script

---
+++ WASMAOT/compile.py
@@ -0,0 +1,46 @@
+from sys import argv, exit
+from os import mkdir, chdir
+from subprocess import call
+
+# Check arguments
+if len(argv) != 4:
+    print(f"Usage: {argv[0]} <WASM module> <extra export,...> <has _initialize function?>")
+    exit(-1)
+
+# Get arguments
+def str_to_bool(v):
+    return v.lower() in ["yes", "true", "1"]
+module = argv[1]
+extra_exports = argv[2].split(',')
+if len(extra_exports) == 1 and len(extra_exports[0]) == 0:
+    extra_exports = []
+has_initialize_func = str_to_bool(argv[3])
+
+# Read template file to get main source
+print("Filling source template...")
+main_source = open("wasmaot.cpp-template").read()
+extra_exports_str = ""
+for export in extra_exports:
+    extra_exports_str += f"EXPORT_FUNC({export})\n"
+main_source = main_source.replace("ADD_extra_exports_here", extra_exports_str)
+main_source = main_source.replace("ADD_has_initialize_func_here", "1" if has_initialize_func else "0")
+
+# Write source file
+open("wasmaot.cpp", "w").write(main_source)
+
+# Execute wasm2c
+print("Converting WASM to C...")
+call(["wasm2c", "-n", "Module", "-o", "Module.c", "--enable-exceptions", module])
+
+# Prepare build dir
+print("Preparing build dir...")
+call(["rm", "-rf", "build-aot"])
+mkdir("build-aot")
+chdir("build-aot")
+
+# Run CMake and compiler
+print("Compiling DLL...")
+call(["cmake", "..", "-GNinja", "-DCMAKE_BUILD_TYPE=Release", "-DCMAKE_C_COMPILER=clang-19", "-DCMAKE_CXX_COMPILER=clang++-19", "-DCMAKE_EXE_LINKER_FLAGS:STRING=-fuse-ld=lld", "-DCMAKE_CXX_FLAGS:STRING=--target=x86_64-windows-msvc", "-DCMAKE_C_FLAGS:STRING=--target=x86_64-windows-msvc", "-DCMAKE_SYSTEM_NAME:STRING=Windows"])
+call(["ninja"])
+
+print("DLL has been generated at build-aot/WASMAOT.dll if everything went well!")
--- WASMExamples/AssemblyScript/assembly/wasm_interface.ts
+++ WASMExamples/AssemblyScript/assembly/wasm_interface.ts
@@ -3,52 +3,94 @@ export type MethodHandle = i32;
 export type WIBool = i32;
 export const unknownArgCount: i32 = 0x6D616E63;
 
+@external("env", "dropObject")
 export declare function dropObject(handle: ObjectHandle): void;
+@external("env", "isValidObject")
 export declare function isValidObject(handle: ObjectHandle): WIBool;
+@external("env", "getNull")
 export declare function getNull(): ObjectHandle;
 
+@external("env", "toCsString")
 export declare function toCsString(str: ArrayBuffer): ObjectHandle;
+@external("env", "toCsStringWithLength")
 export declare function toCsStringWithLength(str: ArrayBuffer, length: i32): ObjectHandle;
+@external("env", "toCString")
 export declare function toCString(str: ObjectHandle, buf: ArrayBuffer, maxlen: i32): void;
 
+@external("env", "logTrace")
 export declare function logTrace(message: ObjectHandle): void;
+@external("env", "logDebug")
 export declare function logDebug(message: ObjectHandle): void;
+@external("env", "logInfo")
 export declare function logInfo(message: ObjectHandle): void;
+@external("env", "logWarn")
 export declare function logWarn(message: ObjectHandle): void;
+@external("env", "logError")
 export declare function logError(message: ObjectHandle): void;
+@external("env", "logCritical")
 export declare function logCritical(message: ObjectHandle): void;
 
+@external("env", "getMethodByIdentifier")
 export declare function getMethodByIdentifier(identifier: ArrayBuffer): MethodHandle;
+@external("env", "getMethodByAddress")
 export declare function getMethodByAddress(address: i64): MethodHandle;
+@external("env", "getMethodName")
 export declare function getMethodName(handle: MethodHandle): ObjectHandle;
+@external("env", "getMethodSignature")
 export declare function getMethodSignature(handle: MethodHandle): ObjectHandle;
 
+@external("env", "addArgI32")
 export declare function addArgI32(v: i32): void;
+@external("env", "addArgI64")
 export declare function addArgI64(v: i64): void;
+@external("env", "addArgFloat")
 export declare function addArgFloat(v: f32): void;
+@external("env", "addArgDouble")
 export declare function addArgDouble(v: f64): void;
+@external("env", "addArgObject")
 export declare function addArgObject(v: ObjectHandle): void;
+@external("env", "addArgNull")
 export declare function addArgNull(): void;
+@external("env", "clearArgs")
 export declare function clearArgs(): void;
+@external("env", "getArgCount")
 export declare function getArgCount(): i32;
+@external("env", "moveArg")
 export declare function moveArg(index: i32): WIBool;
+@external("env", "getValueI32")
 export declare function getValueI32(index: i32): i32;
+@external("env", "getValueI64")
 export declare function getValueI64(index: i32): i64;
+@external("env", "getValueFloat")
 export declare function getValueFloat(index: i32): f32;
+@external("env", "getValueDouble")
 export declare function getValueDouble(index: i32): f64;
+@external("env", "getValueObject")
 export declare function getValueObject(index: i32): ObjectHandle;
+@external("env", "getCallError")
 export declare function getCallError(): ObjectHandle;
+@external("env", "call")
 export declare function call(method: MethodHandle, argCount: i32): WIBool;
+@external("env", "hook")
 export declare function hook(method: MethodHandle, callback: ArrayBuffer): WIBool;
+@external("env", "unhook")
 export declare function unhook(method: MethodHandle): WIBool;
+@external("env", "getOriginal")
 export declare function getOriginal(): MethodHandle;
 
+@external("env", "ImGuiBegin")
 export declare function ImGuiBegin(name: ArrayBuffer): void;
+@external("env", "ImGuiEnd")
 export declare function ImGuiEnd(): void;
+@external("env", "ImGuiText")
 export declare function ImGuiText(text: ObjectHandle): void;
+//@external("env", "ImGuiCheckbox")
 //export declare function ImGuiCheckbox(label: ArrayBuffer, v: ref_any): void;
+@external("env", "ImGuiButton")
 export declare function ImGuiButton(label: ArrayBuffer): WIBool;
+@external("env", "ImGuiSeparator")
 export declare function ImGuiSeparator(): void;
+@external("env", "ImGuiSeparatorText")
 export declare function ImGuiSeparatorText(label: ArrayBuffer): void;
 
 
