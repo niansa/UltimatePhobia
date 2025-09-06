cc1a8b0ca750c373439fa12a504a29d84e49f7c1
niansa <tuxifan@posteo.de>
2025-09-06 02:48:47 +0200

    Use sdlrenderer3

--- UltimatePhobia/imgui_man.cpp
+++ UltimatePhobia/imgui_man.cpp
@@ -3,56 +3,45 @@
 #include "safe_path.hpp"
 
 #include <imgui.h>
-#include <imgui_impl_win32.h>
-#include <imgui_impl_opengl3.h>
-#include <windows.h>
-#include <GL/gl.h>
-
-// Forward declare message handler from imgui_impl_win32.cpp
-extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+#include <imgui_impl_sdl3.h>
+#include <imgui_impl_sdlrenderer3.h>
+#include <SDL3/SDL.h>
 
 namespace ImGuiMan {
 namespace {
 constexpr ImVec4 clear_color = ImVec4(0.1372549019607843f, 0.1215686274509804f, 0.1254901960784314f, 1.00f); // #231f20
 
-struct WGL_WindowData {
-    HDC hDC;
-};
-HGLRC g_hRC = nullptr;
-WGL_WindowData g_MainWindow;
-int g_Width = 0;
-int g_Height = 0;
-
-WNDCLASSEXW wc;
-HWND hwnd;
-
-bool CreateDeviceWGL(HWND hWnd, WGL_WindowData *data);
-void CleanupDeviceWGL(HWND hWnd, WGL_WindowData *data);
-LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+SDL_Window *window = nullptr;
+SDL_Renderer *renderer = nullptr;
 } // namespace
 
 void init() {
     g.logger->info("Initializing GUI...");
 
-    // Create application window
-    // ImGui_ImplWin32_EnableDpiAwareness();
-    wc = {sizeof(wc), CS_OWNDC, WndProc, 0L, 0L, GetModuleHandle(nullptr), nullptr, nullptr, nullptr, nullptr, L"Phasmophobia", nullptr};
-    ::RegisterClassExW(&wc);
-    hwnd = ::CreateWindowW(wc.lpszClassName, L"Phasmophobia", WS_OVERLAPPEDWINDOW, 100, 100, 1280, 800, nullptr, nullptr, wc.hInstance, nullptr);
-
-    // Initialize OpenGL
-    if (!CreateDeviceWGL(hwnd, &g_MainWindow)) {
-        CleanupDeviceWGL(hwnd, &g_MainWindow);
-        ::DestroyWindow(hwnd);
-        ::UnregisterClassW(wc.lpszClassName, wc.hInstance);
-        g.logger->error("Failed to create OpenGL context");
+    // Initialize SDL
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_GAMEPAD) != 0) {
+        g.logger->error("SDL_Init Error: {}", SDL_GetError());
         return;
     }
-    wglMakeCurrent(g_MainWindow.hDC, g_hRC);
 
-    // Show the window
-    ::ShowWindow(hwnd, SW_SHOWDEFAULT);
-    ::UpdateWindow(hwnd);
+    // Create window with SDL_Renderer graphics context
+    SDL_WindowFlags window_flags = SDL_WINDOW_RESIZABLE | SDL_WINDOW_HIGH_PIXEL_DENSITY;
+    window = SDL_CreateWindow("Phasmophobia", 1280, 800, window_flags);
+
+    if (!window) {
+        g.logger->error("SDL_CreateWindow Error: {}", SDL_GetError());
+        SDL_Quit();
+        return;
+    }
+
+    // Create renderer
+    renderer = SDL_CreateRenderer(window, nullptr);
+    if (!renderer) {
+        g.logger->error("SDL_CreateRenderer Error: {}", SDL_GetError());
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return;
+    }
 
     // Setup Dear ImGui context
     IMGUI_CHECKVERSION();
@@ -62,47 +51,50 @@ void init() {
     io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard; // Enable Keyboard Controls
     io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;  // Enable Gamepad Controls
 
-    // Setup Dear ImGui style
-    ImGui::StyleColorsDark();
-
     // Setup Platform/Renderer backends
-    ImGui_ImplWin32_InitForOpenGL(hwnd);
-    ImGui_ImplOpenGL3_Init();
+    ImGui_ImplSDL3_InitForSDLRenderer(window, renderer);
+    ImGui_ImplSDLRenderer3_Init(renderer);
+
+    // Set window position and show
+    SDL_SetWindowPosition(window, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED);
+    SDL_ShowWindow(window);
 }
 
 void deinit() {
     g.logger->info("Deinitializing GUI...");
 
     // Cleanup
-    ImGui_ImplOpenGL3_Shutdown();
-    ImGui_ImplWin32_Shutdown();
+    ImGui_ImplSDLRenderer3_Shutdown();
+    ImGui_ImplSDL3_Shutdown();
     ImGui::DestroyContext();
 
-    CleanupDeviceWGL(hwnd, &g_MainWindow);
-    wglDeleteContext(g_hRC);
-    ::DestroyWindow(hwnd);
-    ::UnregisterClassW(wc.lpszClassName, wc.hInstance);
+    if (renderer)
+        SDL_DestroyRenderer(renderer);
+    if (window)
+        SDL_DestroyWindow(window);
+    SDL_Quit();
 }
 
 bool pre_update() {
-    // Poll and handle messages (inputs, window resize, etc.)
-    MSG msg;
-    while (::PeekMessage(&msg, nullptr, 0U, 0U, PM_REMOVE)) {
-        ::TranslateMessage(&msg);
-        ::DispatchMessage(&msg);
-        if (msg.message == WM_QUIT)
+    // Poll and handle events
+    SDL_Event event;
+    while (SDL_PollEvent(&event)) {
+        ImGui_ImplSDL3_ProcessEvent(&event);
+        if (event.type == SDL_EVENT_QUIT)
+            return false;
+        if (event.type == SDL_EVENT_WINDOW_CLOSE_REQUESTED && event.window.windowID == SDL_GetWindowID(window))
             return false;
     }
 
     // Handle minimized window
-    if (::IsIconic(hwnd)) {
-        ::Sleep(10);
+    if (SDL_GetWindowFlags(window) & SDL_WINDOW_MINIMIZED) {
+        SDL_Delay(10);
         return false;
     }
 
     // Start the Dear ImGui frame
-    ImGui_ImplOpenGL3_NewFrame();
-    ImGui_ImplWin32_NewFrame();
+    ImGui_ImplSDLRenderer3_NewFrame();
+    ImGui_ImplSDL3_NewFrame();
     static std::string iniFilename = (SafePath::get() / "imgui.ini").string();
     ImGui::GetIO().IniFilename = iniFilename.c_str();
     ImGui::NewFrame();
@@ -113,63 +105,13 @@ bool pre_update() {
 void post_update() {
     // Rendering
     ImGui::Render();
-    glViewport(0, 0, g_Width, g_Height);
-    glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
-    glClear(GL_COLOR_BUFFER_BIT);
-    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
-    // Present
-    ::SwapBuffers(g_MainWindow.hDC);
+    ImGuiIO& io = ImGui::GetIO();
+    SDL_SetRenderScale(renderer, io.DisplayFramebufferScale.x, io.DisplayFramebufferScale.y);
+    SDL_SetRenderDrawColorFloat(renderer, clear_color.x, clear_color.y, clear_color.z, clear_color.w);
+    SDL_RenderClear(renderer);
+
+    ImGui_ImplSDLRenderer3_RenderDrawData(ImGui::GetDrawData(), renderer);
+    SDL_RenderPresent(renderer);
 }
-
-namespace {
-bool CreateDeviceWGL(HWND hWnd, WGL_WindowData *data) {
-    HDC hDc = ::GetDC(hWnd);
-    PIXELFORMATDESCRIPTOR pfd = {0};
-    pfd.nSize = sizeof(pfd);
-    pfd.nVersion = 1;
-    pfd.dwFlags = PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER;
-    pfd.iPixelType = PFD_TYPE_RGBA;
-    pfd.cColorBits = 32;
-
-    const int pf = ::ChoosePixelFormat(hDc, &pfd);
-    if (pf == 0)
-        return false;
-    if (::SetPixelFormat(hDc, pf, &pfd) == FALSE)
-        return false;
-    ::ReleaseDC(hWnd, hDc);
-
-    data->hDC = ::GetDC(hWnd);
-    if (!g_hRC)
-        g_hRC = wglCreateContext(data->hDC);
-    return true;
-}
-
-void CleanupDeviceWGL(HWND hWnd, WGL_WindowData *data) {
-    wglMakeCurrent(nullptr, nullptr);
-    ::ReleaseDC(hWnd, data->hDC);
-}
-
-LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) {
-    if (ImGui_ImplWin32_WndProcHandler(hWnd, msg, wParam, lParam))
-        return true;
-
-    switch (msg) {
-    case WM_SIZE:
-        if (wParam != SIZE_MINIMIZED) {
-            g_Width = LOWORD(lParam);
-            g_Height = HIWORD(lParam);
-        }
-        return 0;
-    case WM_SYSCOMMAND:
-        if ((wParam & 0xfff0) == SC_KEYMENU) // Disable ALT application menu
-            return 0;
-        break;
-    case WM_DESTROY:
-        ::PostQuitMessage(0);
-        return 0;
-    }
-    return ::DefWindowProcW(hWnd, msg, wParam, lParam);
-}
-} // namespace
 } // namespace ImGuiMan
